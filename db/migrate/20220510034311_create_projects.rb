6cbccba2a16bed76c16f6b99b359b8dfa474dc0e
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-05-09 23:43:25 -0400

    Create project model

---
+++ db/migrate/20220510034311_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.string :repo
+      t.string :live
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/projects.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :project do
+    name { "MyString" }
+    description { "MyText" }
+    repo { "MyString" }
+    live { "MyString" }
+    image { "MyString" }
+  end
+end
